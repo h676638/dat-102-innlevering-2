4dfab30ac9cdc9a0ce1cc7bd86fc68bc172d6114
678139 <678139@stud.hvl.no>
2024-02-16 11:33:30 +0100

    Update SorteringsMetoder.java

--- oppgave/src/no/hvl/dat102/oblig2/del2/SorteringsMetoder.java
+++ oppgave/src/no/hvl/dat102/oblig2/del2/SorteringsMetoder.java
@@ -3,53 +3,39 @@ package no.hvl.dat102.oblig2.del2;
 import java.util.Arrays;
 import java.util.Random;
 public class SorteringsMetoder {
-
 	public static void main(String[] args) {
-		
-		
-
-
-	public static void main(String[] args) {
-		Integer array[] = new Integer[500000];
+		 int array[] = new int[532000];
 
 		Random random = new Random();
 		for (int i = 0; i < array.length; i++) {
-			array[i] = random.nextInt(5000000);
+			array[i] = random.nextInt(532000);
 
 		}
 		long startTime = System.currentTimeMillis();
-		x(array, 0, 0);
+		mergeSort(array,0,array.length -1);
 		long endTime = System.currentTimeMillis();
 		System.out.println("Sorted array: " + Arrays.toString(array));
 		System.out.println("Time taken to sort  elements: " + (endTime - startTime) + " ms");
 
 	}
-		
-		
-		
-		
-		
-		
-		
-	}
+
+	
 
 	public static void insertionSort(Integer[] arr) {
-			
-			int n = arr.length;
-			
-			 for( i = 0; i < n-1;  i++) {
-				 
-				 Integer k = arr[i];
-				 int j = i-1;
-				 
-				 while (j<=0 && i arr[j]<k) {
-					 arr[j+1] = arr[j];
-					 j = j-1;
-				 }
-				 arr[j+] = k;
-			 }
-			
-		}
+	    int n = arr.length;
+
+	    for (int i = 1; i < n; i++) {
+	        Integer k = arr[i];
+	        int j = i - 1;
+
+	        while (j >= 0 && arr[j] > k) {
+	            arr[j + 1] = arr[j];
+	            j = j - 1;
+	        }
+	        arr[j + 1] = k;
+	    }
+	}
+
 
 	public static void selectionSort(Integer[] arr) {
 
@@ -71,48 +57,35 @@ public class SorteringsMetoder {
 	}
 
 	public static void quickSort(Integer[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
+       if (low < high) {
+           int pi = partition(arr, low, high);
 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
+           quickSort(arr, low, pi - 1);
+           quickSort(arr, pi + 1, high);
+       }
+   }
 
-    private static int partition(Integer[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
+   private static int partition(Integer[] arr, int low, int high) {
+       int pivot = arr[high];
+       int i = low - 1;
+       for (int j = low; j < high; j++) {
+           if (arr[j] < pivot) {
+               i++;
 
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+           }
+       }
 
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
+       int temp = arr[i + 1];
+       arr[i + 1] = arr[high];
+       arr[high] = temp;
 
-        return i + 1;
-    }
+       return i + 1;
+   }
 
 	
-
-	public static void mergeSort(Integer[], lift, right) {
-			if(left < rigth) {
-			 int mid = (left + right)/ 2;
-			  merge( arr, left , mid);
-			  merge(arr, mid+1, right);
-			  
-			  merge(arr, left, mid, right);
-			 
-			}
-			
-		}
-
 
 	public static void mergeSort(int[] arr, int left, int right) {
 		  
@@ -179,4 +152,7 @@ public class SorteringsMetoder {
 			k++;
 			
 		}
+
+	
+}
 }
